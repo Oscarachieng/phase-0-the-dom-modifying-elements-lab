ad98d9989d20346ef724841e3c95ff0cff2ed662
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-12 22:28:34 +0300

    commit tree 63c2fce40f39e84bb063d60f7738dd63c8eb6ad0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('#main');
+main.remove();
+
+let newHeader = document.createElement('H1')
+document.body.append(newHeader);
+
+newHeader.id= 'victory';
+newHeader.innerHTML =  "Oscar is the champion";
